fc768ba5f15d0974cdb29460ae70c417d43664d3
voplotitel <antonkultaew@gmail.com>
2023-07-27 12:57:02 +0200

    first relax

---
+++ waves/waves.js
@@ -0,0 +1,38 @@
+const button = document.querySelector("#myButton");
+
+button.addEventListener("click", () => {
+    document.querySelector("#player").play;
+    document.querySelector("#myVideo").play;
+})
+
+const timer = 10;
+let amountTime = timer * 60;
+
+function calculateTime() {
+    const countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+
+    if (amountTime < 0) {
+        stopTimer();
+        amountTime = 0;
+    }
+
+    function stopTimer() {
+        clearInterval(timerID);
+    }
+}
+    
+
+let timerID = setInterval(calculateTime, 1000);
